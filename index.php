3b318c72766bb7fcdc79fea9c7863c35df26fa72
Jaco Steyn <jacosteyn1990@gmail.com>
2020-08-04 19:33:34 +0200

    Initial commit
    Skeleton html

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>A Blog App</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	</head>
+	<body>
+		<h1>Blog Title</h1>
+		<p>This is a paragraph that summarises what the blog is about.</p>
+
+		<h2>Article 1 title</h2>
+		<div>dd Mon YYYY</div>
+		<p>A paragraph summarising article 1.</p>
+		<p><a href="#">Read more...</a></p>
+
+		<h2>Article 2 title</h2>
+		<div>dd Mon YYYY</div>
+		<p>A paragraph summarising article 2.</p>
+		<p><a href="#">Read more...</a></p>
+	</body>
+</html>
